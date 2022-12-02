bb519d553471085888818e4d6435fb6e5ba97b78
Ritesh Shrestha <text.ritesh@gmail.com>
2022-12-02 13:58:02 +0100

    Add redis CRUD template

--- implementation-redis/main.py
+++ implementation-redis/main.py
@@ -1,72 +1,70 @@
-from flask import Flask, render_template, redirect, url_for
-from flask_wtf import Form, FlaskForm
-from wtforms import IntegerField, SubmitField, TextAreaField, StringField
-import redis, json
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField
+import redis
 from wtforms.validators import DataRequired
 
 # config system
 app = Flask(__name__)
-# Flask-WTF requires an encryption key - the string can be anything
 app.config.update(dict(SECRET_KEY='mysecretkey'))
-r = redis.StrictRedis(host='localhost', port=6379, db=0)
+db = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 # set auto-generated key
-if not r.exists('id'):
-    r.set('id', '0')
-
+if not db.exists('id'):
+    db.set('id', '0')
 
 
 class CreateTask(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
-"""
-
-class CreateTask(Form):
-    title = TextAreaField('Task Title')
-    short_desc = TextAreaField('Short Description')
-    priority = IntegerField('Priority')
+    title = StringField('Task Title', validators=[DataRequired()])
+    shortdesc = StringField('Short Description', validators=[DataRequired()])
+    priority = IntegerField('Priority', validators=[DataRequired()])
     create = SubmitField('Create')
 
 
-class DeleteTask(Form):
-    key = TextAreaField('Task Key')
-    title = TextAreaField('Task Title')
+class DeleteTask(FlaskForm):
+    key = StringField('Task Key')
+    title = StringField('Task Title')
     delete = SubmitField('Delete')
 
 
-class UpdateTask(Form):
-    key = TextAreaField('Task Key')
-    title = TextAreaField('Task Title')
-    short_desc = TextAreaField('Short Description')
-    priority = IntegerField('Priority')
+class UpdateTask(FlaskForm):
+    key = StringField('Task Key', validators=[DataRequired()])
+    title = StringField('Task Title', validators=[DataRequired()])
+    shortdesc = StringField('Short Description', validators=[DataRequired()])
+    priority = IntegerField('Priority', validators=[DataRequired()])
     update = SubmitField('Update')
 
 
-class ResetTask(Form):
+class ResetTask(FlaskForm):
     reset = SubmitField('Reset')
 
 
-def create_task(form):
+def createTask(form):
     title = form.title.data
     priority = form.priority.data
-    short_desc = form
-    task = {'title': title, 'short_desc': short_desc, 'priority': priority}
+    shortdesc = form.shortdesc.data
+    task = {'title': title, 'shortdesc': shortdesc, 'priority': priority}
 
     # set auto-generated key
-    r.hset('T' + str(r.get('id')), task)
-    r.incr('id')
+    task_id = "Task_" + db.get('id').decode('UTF-8')
+    db.hset(task_id, mapping=task)
+    db.incr('id')  # Increment the index
+
     return redirect('/')
 
 
 def deleteTask(form):
     key = form.key.data
     title = form.title.data
-    if (key):
-        r.delete(key)
+
+    if key:
+        db.delete(key)
     else:
-        for i in r.keys():
-            if i != 'id' and r.hget(i, 'title') == title:
+        for i in db.keys():
+            if i != 'id' and db.hget(i, 'title') == title:
                 print(i)
-                r.delete(i)
+                db.delete(i)
+
     return redirect('/')
 
 
@@ -74,18 +72,19 @@ def updateTask(form):
     title = form.title.data
     priority = form.priority.data
     key = form.key.data
-    short_desc = form.shortdesc.data
-    task = {'title': title, 'shortdesc': short_desc, 'priority': priority}
+    shortdesc = form.shortdesc.data
+    task = {'title': title, 'shortdesc': shortdesc, 'priority': priority}
 
     # update by "reset"
-    if (r.exists(key)):
-        r.hset(key, task)
+    if db.exists(key):
+        db.hset(key, task)
+
     return redirect('/')
 
 
 def resetTask(form):
-    r.flushall()
-    r.set('id', '0')
+    db.flushall()
+    db.set('id', '0')
     return redirect('/')
 
 
@@ -98,28 +97,24 @@ def main():
     reset = ResetTask(prefix='reset')
 
     # response
-   if cform.create.data:
-       return createTask(cform)
-   # if dform.validate_on_submit() and dform.delete.data:
-       # return deleteTask(dform)
-    #if uform.validate_on_submit() and uform.update.data:
-      #  return updateTask(uform)
-    #if reset.validate_on_submit() and reset.reset.data:
-     #   return resetTask(reset)
+    if cform.validate_on_submit() and cform.create.data:
+        return createTask(cform)
+    if dform.validate_on_submit() and dform.delete.data:
+        return deleteTask(dform)
+    if uform.validate_on_submit() and uform.update.data:
+        return updateTask(uform)
+    if reset.validate_on_submit() and reset.reset.data:
+        return resetTask(reset)
 
     # get all data
-    keys = r.keys()
+    keys = db.keys()
     val = {}
     for i in keys:
         if i != 'id':
-            val[i] = r.hgetall(i)
-
-    return render_template('home.html', cform=cform, dform=dform, uform=uform, keys=keys, val=val, reset=reset)
+            val[i] = 'TEST'
+    return render_template('home.html', cform=cform, dform=dform, uform=uform,
+                           keys=keys, val=val, reset=reset)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-    """
-
