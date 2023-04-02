ad41ffd7b324157500669f2dc6add2c2aacf6edb
Ritesh Shrestha <text.ritesh@gmail.com>
2023-04-02 16:46:21 +0200

    Update report profiling and change configs M4

--- middleware/tests/flush_operation.py
+++ middleware/tests/flush_operation.py
@@ -2,7 +2,7 @@ from main import MWare
 
 if __name__ == '__main__':
     m_ware = MWare()
-    m_ware.flush_all()
+    # m_ware.flush_all()
     print(m_ware.key_space_inf())
     """
     u_ids = []
