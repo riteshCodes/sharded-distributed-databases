4f5a96cbdb4220cee95efe4adeab8636cbb80486
Ritesh Shrestha <text.ritesh@gmail.com>
2023-04-28 00:35:17 +0200

    Add discussion latex

--- middleware/configs.py
+++ middleware/configs.py
@@ -2,7 +2,7 @@ ALL_NODES = ["redis://:sharding-ddms@10.0.2.81:6379/0", "redis://:sharding-ddms@
              "redis://:sharding-ddms@10.0.2.83:6379/0", "redis://:sharding-ddms@10.0.2.84:6379/0",
              "redis://:sharding-ddms@10.0.2.85:6379/0", "redis://:sharding-ddms@10.0.2.86:6379/0"]
 DB_NODES = ALL_NODES[:6]
-VIRTUAL_NODES = 36
+VIRTUAL_NODES = 320
 # Uniform distribution of keys depends upon the number of virtual nodes (directly proportional)
 # But the number of virtual nodes has also negative impact upon the response time (due to computational overhead
 # of searching keys and their respective nodes
